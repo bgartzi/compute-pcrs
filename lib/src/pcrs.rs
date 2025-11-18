eca0669582592bb2f4f7befa632d412c3fac83f8
Beñat Gartzia Arruabarrena <bgartzia@redhat.com>
2025-11-18 17:58:37 +0100

    pcrs: Support heterogeneous pcr# vector compilation
    Until now, compiling TPMEvents into PCRs assumed that all TPMEvents were
    from the same PCR.
    
    This patch adds a check to the Pcr::compile_from method so it checks
    that all events processed belong to the same PCR.
    
    Based on that, it adds another function that computes vectors of Pcrs
    from an heterogeneous TPMEvent vector.
    
    Finally it adds a few slight tests to cover the new functionality and
    updates some old ones that for some reason assumed that having different
    pcr events compiled under the same compile_from call was right.
    
    Signed-off-by: Beñat Gartzia Arruabarrena <bgartzia@redhat.com>

--- lib/src/pcrs.rs
+++ lib/src/pcrs.rs
@@ -4,6 +4,7 @@
 // SPDX-License-Identifier: MIT
 
 use crate::tpmevents::TPMEvent;
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 use sha2::{Digest, Sha256};
@@ -44,10 +45,22 @@ impl From<&TPMEvent> for Part {
 }
 
 impl Pcr {
+    /// Only supports compiling PCRs from vectors of events that belong
+    /// to the same PCR
+    /// Note that events must be ordered per PCR and the order TPM would
+    /// expect them to be logged
     pub fn compile_from(events: &Vec<TPMEvent>) -> Pcr {
         let mut result = PCR_INIT_VALUE.to_vec();
+        let compiled_pcr: u8 = events[0].pcr;
 
         for event in events {
+            if event.pcr != compiled_pcr {
+                // FIXME: better error handling
+                panic!(
+                    "unexpected pcr#{} while compiling pcr#{}",
+                    event.pcr, compiled_pcr
+                );
+            }
             let mut hasher = Sha256::new();
             hasher.update(result);
             hasher.update(event.hash.clone());
@@ -60,4 +73,24 @@ impl Pcr {
             parts: events.iter().map(|e| e.into()).collect(),
         }
     }
+}
+
+/// Supports compiling vectors of PCRs from vectors of events that belong
+/// to different PCRs
+/// Note that events must be ordered per PCR and the order TPM would
+/// expect them to be logged
+pub fn compile_pcrs(events: &Vec<TPMEvent>) -> Vec<Pcr> {
+    let pcrs: Vec<u8> = events.iter().map(|e| e.pcr).unique().collect();
+
+    pcrs.iter()
+        .map(|n| {
+            Pcr::compile_from(
+                &events
+                    .iter()
+                    .filter(|e| e.pcr == *n)
+                    .map(|e| e.clone())
+                    .collect(),
+            )
+        })
+        .collect()
 }
